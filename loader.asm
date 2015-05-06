1ac32d7d4319070d3b2f18208342f4f8032a62d6
Samuel Moritz <samuel.moritz@gmail.com>
2015-05-06 21:07:33 +0200

    Easier constants. Nicer OS

--- loader.asm
+++ loader.asm
@@ -1,4 +1,4 @@
-; CPU starts in "real mode"
+; Every x86 CPU starts in "real mode"
 ; BIOS loads the bootloader into memory at 0x7C00
 ; Bootloader must be 512 bytes
 ; Bootloader must end with signature bytes: 0x55,0xAA
@@ -8,13 +8,8 @@ use16
 org 0x7c00
 
 boot:
-    ; initialize segment registers
-    xor ax, ax
-    mov ds, ax
-    mov es, ax
-    mov ss, ax
-    ; load rust code into 0x7e00 so we can jump to it later
-    mov bx, 0x7e00  ; read buffer
+    ; load OS kernel into 0x7e00 so we can jump to it later
+    mov bx, 0x8000  ; read buffer
     mov ah, 2   ; read
     mov al, 1   ; 1 sector at a time (512B)
     mov ch, 0   ; cylinder
@@ -24,10 +19,7 @@ boot:
     int 0x13
     jc error
 
-    ; initialize stack
-    mov sp, 0x7bfe
-
-    jmp 0x7e00
+    jmp 0x8000
 
 error:
     mov si, .msg
@@ -40,7 +32,7 @@ error:
     jmp .loop
 .done:
     jmp $
-    .msg db "rOSt: could not read disk", 0
+    .msg db "Error: could not read disk", 0
 
 times 510-($-$$) db 0
 db 0x55
--- os.asm
+++ os.asm
@@ -1,6 +1,6 @@
 use16
 
-org 0x7e00
+org 0x8000
 
 main:
     xor ax, ax
@@ -12,13 +12,14 @@ main:
     mov dx, 184fh
     int 10h
 
-    xor ax, ax
+    mov al, 0
     mov ah, 2
     xor bx, bx
     xor dx, dx
     int 10h
 
-    mov si, .msg
+greeting:
+    mov si, msg
 .loop:
     lodsb
     or al, al
@@ -27,6 +28,26 @@ main:
     int 0x10
     jmp .loop
 .done:
+
+terminal:
+    mov al, 0
+    mov ah, 2
+    xor bx, bx
+    xor dx, dx
+    mov dh, 1
+    int 10h
+
+    mov si, prompt
+.loop:
+    lodsb
+    or al, al
+    jz .done
+    mov ah, 0x0e
+    int 0x10
+    jmp .loop
+.done:
+
     jmp $
 
-.msg db "This is the operating system!", 0
+msg db "This is the operating system!", 0
+prompt db "$ ", 0
