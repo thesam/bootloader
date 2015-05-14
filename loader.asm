28bd1a62254ae92ecad0c39d1bd5e6419591d687
Samuel Moritz <samuel.moritz@gmail.com>
2015-05-14 21:51:59 +0200

    Swedish comments for presentation

--- loader.asm
+++ loader.asm
@@ -1,22 +1,20 @@
-; Every x86 CPU starts in "real mode"
-; BIOS loads the bootloader into memory at 0x7C00
-; Bootloader must be 512 bytes
-; Bootloader must end with signature bytes: 0x55,0xAA
-
-use16
+; En bootladdare måste sluta med 0x55,0xaa
+; En bootladdare måste vara 512 bytes
+; Sector 1: Bootladdare
+; Sector 2: Operativsystemet
+; Bootladdaren laddas in till 0x7c00
 
 org 0x7c00
 
-boot:
-    mov bx, 0x8000  ; read buffer
-    mov ah, 2   ; read
-    mov al, 1   ; 1 sector at a time (512B)
-    mov ch, 0   ; cylinder
-    mov cl, 2   ; sector
-    mov dh, 0  ; head
-    int 0x13
+mov bx, 0x8000 ; mål
+mov ah, 2 ; 2 = read from disk
+mov al, 1 ; 1 = antal sektorer att läsa
+mov ch, 0 ; cylinder
+mov cl, 2 ; sector 2 (se ovan)
+mov dh, 0 ; head
+int 13h ; Anropa BIOS via interrupt
 
-    jmp 0x8000
+jmp 0x8000
 
 times 510-($-$$) db 0
 db 0x55
